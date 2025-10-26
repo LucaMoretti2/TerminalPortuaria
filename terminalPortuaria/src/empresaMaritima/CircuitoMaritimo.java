443bdedd934dd0180da98faf6c1b8ea0e9d9bc11
Camila <arenabelu@gmail.com>
2025-10-26 11:03:03 -0300

    Se actualizo CircuitoMaritimo

--- terminalPortuaria/src/empresaMaritima/CircuitoMaritimo.java
+++ terminalPortuaria/src/empresaMaritima/CircuitoMaritimo.java
@@ -1,5 +1,53 @@
 package empresaMaritima;
 
-public class CircuitoMaritimo {
+	import java.util.ArrayList;
+	import java.util.List;
 
-}
+	import tramo.Tramo;
+
+	public class CircuitoMaritimo {
+		    private String nombre;
+		    private List<Tramo> tramos;
+
+		
+		    public CircuitoMaritimo(String nombre) {
+		        this.nombre = nombre;
+		        this.tramos = new ArrayList<>();
+		    }
+
+		   
+		    public String getNombre() {
+		        return nombre;
+		    }
+
+		    public List<Tramo> getTramos() {
+		        return tramos;
+		    }
+
+
+		    public void agregarTramo(Tramo tramo) {
+		        tramos.add(tramo);
+		    }
+
+
+		    public long calcularTiempoTotalHoras() {
+		        long total = 0;
+		        for (Tramo tramo : tramos) {
+		            total += tramo.getDuracionEnHoras();
+		        }
+		        return total;
+		    }
+
+
+		    public double calcularPrecioTotal() {
+		        double total = 0;
+		        for (Tramo tramo : tramos) {
+		            total += tramo.getPrecio();
+		        }
+		        return total;
+		    }
+
+		    // Falta mostrar el circuito
+	}
+
+
